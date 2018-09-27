86bcbeb7069a07829b77d53b23ec1ac1328edea3
ShubhamGupta0 <43651311+ShubhamGupta0@users.noreply.github.com>
2018-09-27 23:23:20 +0530

    Update PositionFileModel.java

--- PositionFileModel.java
+++ PositionFileModel.java
@@ -1,1 +1,42 @@
 
+package model;
+
+import com.google.gson.annotations.SerializedName;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class TransactionModel {
+   
+
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public int getTransactionQuantity() {
+        return transactionQuantity;
+    }
+
+    public void setTransactionQuantity(int transactionQuantity) {
+        this.transactionQuantity = transactionQuantity;
+    }
+}
