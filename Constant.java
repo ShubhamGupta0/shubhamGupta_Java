407157298e8eb25596dd848ffd79a81192802eaf
ShubhamGupta0 <43651311+ShubhamGupta0@users.noreply.github.com>
2018-09-27 23:11:54 +0530

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Constant {
+    public static final String START_OF_DAY_POSITION_FILE_PATH = "../../../../Coding_Assignment/src/main/resources/files/Input_StartOfDay_Positions.csv";
+    public static final String END_OF_DAY_POSITION_FILE_PATH = "../../../../Coding_Assignment/src/main/resources/files/Expected_EndOfDay_Positions.txt.csv";
+    public static final String TRANSACTION_FILE_PATH= "../../../../Coding_Assignment/src/main/resources/files/1537277231233_Input_Transactions.txt";
+    public static final int START_OF_DAY_POSITION_FILECOLUMN_COUNT =4;
+    public static final int TRANSACTION_FILECOLUMN_COUNT =4;
+}
